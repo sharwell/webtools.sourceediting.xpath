15b2c1b629293fdc186fcd387bbf89473ddfa80b
nitind <nitind>
2011-04-20 12:40:00 +0000

    This commit was manufactured by cvs2svn to create tag 'v201104201240'.

--- bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/internal/types/builtin/SingleItemSequence.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.wst.xml.xpath2.processor.internal.types.builtin;
-
-import org.eclipse.wst.xml.xpath2.api.Item;
-import org.eclipse.wst.xml.xpath2.api.ResultSequence;
-
-public interface SingleItemSequence extends Item, ResultSequence {
-
-}

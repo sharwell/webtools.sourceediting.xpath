e3f0b5a9c872479982956c0bc90a4aad1e4161d2
Sam Harwell <sam@tunnelvisionlabs.com>
2016-07-04 08:36:33 -0500

    Fix handling of time 24:00 in xs:time and xs:dateTime
    Fixes #145
    Fixes #146

--- bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/internal/types/XSDateTime.java
+++ bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/internal/types/XSDateTime.java
@@ -296,10 +296,6 @@ Cloneable {
 
 		ret[2] = Double.parseDouble(token);
 
-		if (ret[0] == 24.0) {
-			ret[0] = 00.0;
-		}
-
 		// XXX sanity check args...
 		return ret;
 	}
@@ -460,8 +456,17 @@ Cloneable {
 		if (t == null)
 			return null;
 
-		if (!set_item(cal, Calendar.HOUR_OF_DAY, (int) t[0]))
+		if (t[0] == 24) {
+			// '24' is permitted if the minutes and seconds represented are zero
+			if (t[1] != 0 || t[2] != 0) {
+				return null;
+			}
+
+			cal.set(Calendar.HOUR_OF_DAY, 0);
+			cal.add(Calendar.HOUR_OF_DAY, 24);
+		} else if (!set_item(cal, Calendar.HOUR_OF_DAY, (int) t[0])) {
 			return null;
+		}
 
 		if (!set_item(cal, Calendar.MINUTE, (int) t[1]))
 			return null;
